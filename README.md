c33c6b0cd575915934d977abfb7be8da8e48f19f
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-05-01 19:02:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pong
+ JavaScript Pong
---
+++ index.js
@@ -0,0 +1,4 @@
+const gameBoard = document.querySelector('#gameBoard');
+const ctx = gameBoard.getContext('2d');
+const scoreText = document.querySelector('#scoreText');
+const resetBtn = document.querySelector('#resetBtn');
