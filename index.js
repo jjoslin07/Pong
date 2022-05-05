ce4ed6893781e387d71af7fed4537c6c1387a9b7
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-05-04 18:17:43 -0700

    final

--- index.js
+++ index.js
@@ -66,10 +66,69 @@ function drawPaddles() {
 	ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 	ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 }
-function createBall() {}
-function moveBall() {}
-function drawBall(ballX, ballY) {}
-function checkCollision() {}
+function createBall() {
+	ballSpeed = 1;
+	if (Math.round(Math.random()) == 1) {
+		ballXDirection = 1;
+	} else {
+		ballXDirection = -1;
+	}
+	if (Math.round(Math.random()) == 1) {
+		ballYDirection = 1;
+	} else {
+		ballYDirection = -1;
+	}
+	ballX = gameWidth / 2;
+	ballY = gameHeight / 2;
+	drawBall(ballX, ballY);
+}
+function moveBall() {
+	ballX += ballSpeed * ballXDirection;
+	ballY += ballSpeed * ballYDirection;
+}
+function drawBall(ballX, ballY) {
+	ctx.fillStyle = ballColor;
+	ctx.strokeStyle = ballBorderColor;
+	ctx.lineWidth = 2;
+	ctx.beginPath();
+	ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+	ctx.stroke();
+	ctx.fill();
+}
+function checkCollision() {
+	if (ballY <= 0 + ballRadius) {
+		ballYDirection *= -1;
+	}
+	if (ballY >= gameHeight - ballRadius) {
+		ballYDirection *= -1;
+	}
+	if (ballX <= 0) {
+		player2Score += 1;
+		updateScore();
+		createBall();
+		return;
+	}
+	if (ballX >= gameWidth) {
+		player1Score += 1;
+		updateScore();
+		createBall();
+		return;
+	}
+	if (ballX <= paddle1.x + paddle1.width + ballRadius) {
+		if (ballY > paddle1.y && ballY < paddle1.y + paddle1.height) {
+			ballX = paddle1.x + paddle1.width + ballRadius; // If the ball gets stuck
+			ballXDirection *= -1;
+			ballSpeed += 1;
+		}
+	}
+	if (ballX >= paddle2.x - ballRadius) {
+		if (ballY > paddle2.y && ballY < paddle2.y + paddle2.height) {
+			ballX = paddle2.x - ballRadius; // If the ball gets stuck
+			ballXDirection *= -1;
+			ballSpeed += 1;
+		}
+	}
+}
 function changeDirection(event) {
 	const keyPressed = event.keyCode;
 	const paddle1Up = 87;
@@ -100,5 +159,30 @@ function changeDirection(event) {
 			break;
 	}
 }
-function updateScore() {}
-function resetGame() {}
+function updateScore() {
+	scoreText.textContent = `${player1Score} : ${player2Score}`;
+}
+function resetGame() {
+	player1Score = 0;
+	player2Score = 0;
+	paddle1 = {
+		width: 25,
+		height: 100,
+		x: 0,
+		y: 0,
+	};
+	paddle2 = {
+		width: 25,
+		height: 100,
+		x: gameWidth - 25,
+		y: gameHeight - 100,
+	};
+	ballSpeed = 1;
+	ballX = 0;
+	ballY = 0;
+	ballXDirection = 0;
+	ballYDirection = 0;
+	updateScore();
+	clearInterval(intervalID);
+	gameStart();
+}
